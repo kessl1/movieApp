001df955937870b5d3156ab8ae30637dbb3023e0
Jake Kessel <jake-kessel@uiowa.edu>
2017-03-06 01:02:54 -0600

    added functionality to read from sql server

---
+++ src/java/dbHelpers/ReadQuery.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Movies;
+
+/**
+ *
+ * @author Jake
+ */
+public class ReadQuery {
+    private Connection conn;
+    private ResultSet results;
+    
+    public ReadQuery(){
+        Properties props = new Properties();
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public void doRead(){
+        try {
+            String query = "Select * from MOVIES";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String getHTMLtable(){
+        String table = "";
+        table += "<table border=1>";
+        
+        try {
+            while(this.results.next()){
+                Movies movie = new Movies();
+                movie.setMovieID(this.results.getInt("movieID"));
+                movie.setTitle(this.results.getString("title"));
+                movie.setStudio(this.results.getString("studio"));
+                movie.setYear(this.results.getInt("year"));
+                movie.setGenre(this.results.getString("genre"));
+                movie.setLead(this.results.getString("lead"));
+                
+                table += "<tr>";
+                table += "<td>";
+                table+=movie.getMovieID();
+                table += "</td>";
+                
+                table += "<td>";
+                table+=movie.getTitle();
+                table += "</td>";
+                
+                table += "<td>";
+                table+=movie.getStudio();
+                table += "</td>";
+                
+                table += "<td>";
+                table+=movie.getYear();
+                table += "</td>";
+                
+                table += "<td>";
+                table+=movie.getGenre();
+                table += "</td>";
+                
+                table += "<td>";
+                table+=movie.getLead();
+                table += "</td>";
+                
+                table += "</td>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table+= "</table>";
+        
+        return table;
+    }
+    
+}
---
+++ web/read.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : read
+    Created on : Mar 5, 2017, 10:27:37 PM
+    Author     : Jake
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    
+    <% String table = (String) request.getAttribute("table"); %>
+    
+    <body>
+        <h1>Jake's Movies</h1
+        <%= table %>
+    </body>
+</html>
